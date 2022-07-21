f12b6066e5f43b0cadb8fa0446e5ae8541d5e2c1
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-21 23:54:34 +0900

    ts 공부

---
+++ example/13_type-guard.ts
@@ -0,0 +1,31 @@
+interface Developer{
+  name: string
+  skill : string
+}
+
+interface Person{
+  name: string
+  age : number
+}
+
+function introduce() : Developer | Person{
+  return { name: "John", age: 12 , skill: "web"}
+}
+
+let tony = introduce()
+// console.log(tony.skill) 오류 발생 tony.name만 출력 가능
+
+if((tony as Developer).skill){
+  let skill = (tony as Developer).skill
+  console.log(skill);
+}
+else if((tony as Person).age){
+  let age= ((tony as Person).age);
+  console.log(age);
+}
+
+// 이렇게 복잡한 코드를 간단하게 작성하기 위하여 타입 가드가 나왔다.
+
+function isDeveloper(target : Developer | Person): target is Developer {
+  return( target is Developer)
+}
--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -6,7 +6,7 @@ function logA(a = 'a') {
 }
 
 interface Dropdown<T> {
-  value: T
+  value: T 
   title: string;
 }
 var items: Dropdown<number> = {
@@ -14,13 +14,17 @@ var items: Dropdown<number> = {
   title: 'a'
 }
 
+// DetailedDropdown가 Dropdown을 상속 받는데 DetailedDropdown의 제너릭 타입을 string으로 주면 Dropdown 타입도 string이 된다.
 interface DetailedDropdown<T> extends Dropdown<T> {
   description: string;
   tag: T;
 }
-var detailItems: DetailedDropdown<number> = {
+var detailItems: DetailedDropdown<string> = {
   value: 'hi',
   title: 'a',
   description: 'b',
   tag: 'c'
 }
+
+// Best Common Type : 가장 근접한 타입을 추론한다.
+let arr:(number|boolean)[] =[0,true]
