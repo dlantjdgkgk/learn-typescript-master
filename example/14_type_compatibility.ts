0cf319016efa6c6887949ff08e7b78dfe9f13b60
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-25 13:56:19 +0900

    ts 공부

---
+++ example/14_type_compatibility.ts
@@ -0,0 +1,21 @@
+interface Developer{
+  name :string;
+  skill: string
+}
+
+interface Person2{
+  name :string;
+}
+
+let developer : Developer
+let person2 : Person2
+// developer = person2 이거는 불가능한게 person2 변수는 developer 변수보다 타입이 부족해서 종속이 안된다.
+// person2 = developer 이거는 가능
+
+let add= (a:number)=>{  
+}
+
+let sum = (a:number,b :number)=>{}
+
+sum = add; // 가능
+// add = sum; // 불가능
